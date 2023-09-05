879b03dc1de177891e2c0585dea918204c562654
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2023-09-05 12:06:00 +0530

    change img path for category

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function Category({ category }) {
     const navigate = useNavigate();
     return (
-        <div className='Category' style={{ backgroundImage: `url(${category?.attributes?.image.data.attributes.url})` }} onClick={() => navigate(`category/${category?.attributes?.key}`)}>
+        <div className='Category' style={{ backgroundImage: `url(https://stripe-server-hva23.ondigitalocean.app${category?.attributes?.image.data.attributes.url})` }} onClick={() => navigate(`category/${category?.attributes?.key}`)}>
             <div className="category-content center">
                 <h3 className='heading'>{category?.attributes?.title}</h3>
             </div>
